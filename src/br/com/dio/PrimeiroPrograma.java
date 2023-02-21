4d0d5620f36a4e18b405f2d445907d4fee928a53
eriklisboa1 <eriklisboa72@outlook.com>
2023-02-21 00:27:00 -0300

    first commit in java

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Pessoa;
+
+public class PrimeiroPrograma {
+    public static void main(String[] agrs) {
+
+        Pessoa pessoa = new Pessoa();
+        System.out.println(pessoa);
+/*        int a = 4;
+        int b = 3;
+        System.out.println("ola " +(a + b));*/
+    }
+
+}
